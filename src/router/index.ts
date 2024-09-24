fb669f3f2c17b5de860eff6c81af63943858e995
Santiago Martinez <smartinez9626@gmail.com>
2024-09-24 08:24:13 -0500

    Vue SPA APP

---
+++ src/router/index.ts
@@ -0,0 +1,73 @@
+import NotFound404 from '@/modules/common/pages/NotFound404.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // Landing
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('@/modules/landing/pages/HomePage.vue'),
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landing/pages/PricingPage.vue'),
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/landing/pages/ContactPage.vue'),
+        },
+        {
+          path: '/pokemon/:id',
+          name: 'pokemon',
+          //props: true,
+          props: (route) => {
+            //console.log({ route });
+            const id = Number(route.params.id);
+            console.log({ id });
+            return isNaN(id) ? { id: 1 } : { id };
+          },
+          component: () => import('@/modules/pokemons/pages/PokemonPage.vue'),
+        },
+      ],
+    },
+    // Auth
+    {
+      path: '/auth',
+      redirect: { name: 'login' },
+      component: () => import('@/modules/auth/layouts/AuthLayout.vue'),
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import('@/modules/auth/pages/LoginPage.vue'),
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+        },
+      ],
+    },
+    // Not Found
+    {
+      path: '/:pathMatch(.*)*',
+      component: NotFound404,
+    },
+  ],
+});
+
+export default router;
